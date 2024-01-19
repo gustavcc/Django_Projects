ff1ca742a916dc42a1d57556a2c26c0769ee1b62
Gustavo Cardoso Costa <ritacardoso290@gmail.com>
2024-01-19 11:56:26 -0300

    uptade login_cadastro

--- login_cadastro/app_login/views.py
+++ login_cadastro/app_login/views.py
@@ -4,7 +4,20 @@ from .models import Usuario
 # Create your views here.
 
 def cadastro(request):
-    return render(request, 'app_login/index.html')
+    return render(request, 'app_login/pages/index.html')
 
 def login(request):
-    pass
+    return render(request, 'app_login/pages/login.html')
+
+def dashboard(request):
+    novo_usuario = Usuario()
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.email = request.POST.get('email')
+    novo_usuario.senha = request.POST.get('senha')
+    novo_usuario.save()
+    #! exibir todos os usuarios em uma nova pagina (Django interpreta em dicionario)
+    usuarios = {
+        'usuarios': Usuario.objects.all()
+    }
+    #! retornar os dados para a pagina de listagem
+    return render(request,'app_login/pages/dashboard.html',usuarios)
--- login_cadastro/login_cadastro/urls.py
+++ login_cadastro/login_cadastro/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from app_login import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('',views.cadastro,name='cadastro'),
-    path('login/',views.login,name='login'),
-    path('admin/',admin.site.urls),
-]
+    path('',views.cadastro, name='cadastro'),
+    path('login/',views.login, name='login'),
+    path('dashboard/',views.dashboard, name='dashboard'),
+] 
