72605b92646d0636b219692b73092fb263713b89
Gustavo Cardoso Costa <ritacardoso290@gmail.com>
2024-01-20 11:05:34 -0300

    delete

--- bananas_project/bananas_project/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from paginas import views
-
-urlpatterns = [
-    path('',views.home,name='home'),
-    path('dashboard/',views.cortes,name='dashboard'),
-    path('admin/', admin.site.urls),
-]
--- bananas_project/paginas/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Corte(models.Model):
-    id_corte = models.AutoField(primary_key=True)
-    primeira = models.IntegerField()
-    segunda = models.IntegerField()
-    
