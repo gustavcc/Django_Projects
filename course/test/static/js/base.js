713903f58ae553b6f337d3fcebc785394861b262
Gustavo Cardoso Costa <ritacardoso290@gmail.com>
2024-02-28 21:32:23 -0300

    up course

--- course/test/static/js/base.js
+++ course/test/static/js/base.js
@@ -1,22 +1,19 @@
 const hamburger = document.getElementById('menu-hamburger');
-const menu_activate = document.getElementById('menu-activate');
+const menu = document.getElementById('menu');
 
 hamburger.addEventListener('change', () => {
-    menu_activate.showModal();
     if (hamburger.checked) {
-        menu_activate.style.left = '0';
-        window.addEventListener('click', outsideClickListener);
+        menu.style.left = '0';
+        // window.addEventListener('click', outsideClickListener);
     } else {
-        menu_activate.close();
-        menu_activate.style.left = '-80vw';
-        window.removeEventListener('click', outsideClickListener);
+        menu.style.left = '-80vw';
+        // window.removeEventListener('click', outsideClickListener);
     }
 });
 
-function outsideClickListener(event) {
-    if (event.target === menu_activate) {
-        menu_activate.close();
-        menu_activate.style.left = '-80vw';
-        hamburger.checked = false;
-    }
-};
+const html = document.querySelector('html');
+const dark_mode = document.getElementById('switch');
+
+dark_mode.addEventListener('change', ()=>{
+    html.classList.toggle('dark-mode')
+})
