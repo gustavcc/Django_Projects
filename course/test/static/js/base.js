583ffdba51a5d594b386b9a968df57e402af4619
Gustavo Cardoso Costa <ritacardoso290@gmail.com>
2024-03-06 22:29:44 -0300

    up questions

--- course/test/static/js/base.js
+++ course/test/static/js/base.js
@@ -1,19 +1,20 @@
+// Menu
 const hamburger = document.getElementById('menu-hamburger');
 const menu = document.getElementById('menu');
 
 hamburger.addEventListener('change', () => {
     if (hamburger.checked) {
         menu.style.left = '0';
-        // window.addEventListener('click', outsideClickListener);
     } else {
         menu.style.left = '-80vw';
-        // window.removeEventListener('click', outsideClickListener);
     }
 });
 
+// Dark Mode 
 const html = document.querySelector('html');
 const dark_mode = document.getElementById('switch');
 
 dark_mode.addEventListener('change', ()=>{
     html.classList.toggle('dark-mode')
 })
+
