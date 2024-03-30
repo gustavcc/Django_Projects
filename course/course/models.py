8be37fa4eb7034652b1b9ffdacc10ba1fc6850b0
gustavcc <ritacardoso290@gmail.com>
2024-03-30 11:05:39 -0300

    up

--- course/course/models.py
+++ course/course/models.py
@@ -11,13 +11,16 @@ class Question(models.Model):
     )
     
     title = models.TextField()
+    test = models.TextField()
+    content = models.TextField()
+    ano = models.TextField()
     answer_a = models.CharField(max_length=255, default='')
     answer_b = models.CharField(max_length=255, default='')
     answer_c = models.CharField(max_length=255, default='')
     answer_d = models.CharField(max_length=255, default='')
     answer_e = models.CharField(max_length=255, default='')
     
-    correct = models.CharField(max_length=8, choices=CORRECT)
+    correct = models.CharField(max_length=2, choices=CORRECT)
     
     def __str__(self):
         return self.title
